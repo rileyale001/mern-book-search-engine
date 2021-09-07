39c2756f1d979db1fcdb8f6e1c46e24ba1f21284
rileyale001 <alexjtriley@gmail.com>
2021-09-07 03:39:11 -0700

    committing on main

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,39 +1,19 @@
-// import { gql } from '@apollo/client';
-// export const GET_USER = gql`
-//   {
-//     me {
-//         _id
-//         email
-//         username
-//         bookCount
-//         savedBooks {
-//           bookId
-//           image
-//           authors
-//           description
-//           title
-//           link
-//         }
-//       }
-//     }
-// `;
-import gql from 'graphql-tag';
-
+import { gql } from '@apollo/client';
 export const GET_USER = gql`
   {
     me {
-      _id
-      username
-      email
-      bookCount
-      savedBooks {
-        bookId
-        authors
-        image
-        description
-        title
-        link
+        _id
+        email
+        username
+        bookCount
+        savedBooks {
+          bookId
+          image
+          authors
+          description
+          title
+          link
+        }
       }
     }
-  }
 `;
