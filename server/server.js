ec49444ed033eaef33353569b1a70757221e414a
rileyale001 <alexjtriley@gmail.com>
2021-09-06 22:50:47 -0700

    fixed routes in server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express')
-const routes = require('./routes');
+// const routes = require('./routes');
 const {typeDefs, resolvers} = require('./shemas')
 const { authMiddleware }= require('./utils/auth');
 
